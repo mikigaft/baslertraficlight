248d81ea3acae8d0d7de309a2bcfb639b0d75c62
Miks <miks@groglass.com>
2015-04-29 16:31:59 +0300

    basler db monitor

---
+++ baslertraficlight/Form1.Designer.vb
@@ -0,0 +1,165 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.lstProducts = New System.Windows.Forms.ListBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblCreatedAt = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblSheetCode = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.bSave = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(98, 250)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        Me.Button2.Visible = False
+        '
+        'lstProducts
+        '
+        Me.lstProducts.FormattingEnabled = True
+        Me.lstProducts.Location = New System.Drawing.Point(177, 250)
+        Me.lstProducts.Name = "lstProducts"
+        Me.lstProducts.Size = New System.Drawing.Size(120, 30)
+        Me.lstProducts.TabIndex = 2
+        Me.lstProducts.Visible = False
+        '
+        'Timer1
+        '
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(231, 148)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(66, 50)
+        Me.PictureBox1.TabIndex = 3
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(23, 64)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(60, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Created At:"
+        '
+        'lblCreatedAt
+        '
+        Me.lblCreatedAt.AutoSize = True
+        Me.lblCreatedAt.Location = New System.Drawing.Point(107, 64)
+        Me.lblCreatedAt.Name = "lblCreatedAt"
+        Me.lblCreatedAt.Size = New System.Drawing.Size(56, 13)
+        Me.lblCreatedAt.TabIndex = 5
+        Me.lblCreatedAt.Text = "Time Date"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(23, 51)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(66, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Sheet Code:"
+        '
+        'lblSheetCode
+        '
+        Me.lblSheetCode.AutoSize = True
+        Me.lblSheetCode.Location = New System.Drawing.Point(107, 51)
+        Me.lblSheetCode.Name = "lblSheetCode"
+        Me.lblSheetCode.Size = New System.Drawing.Size(31, 13)
+        Me.lblSheetCode.TabIndex = 7
+        Me.lblSheetCode.Text = "0000"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 25)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(285, 20)
+        Me.TextBox1.TabIndex = 8
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(89, 13)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Connection string"
+        '
+        'bSave
+        '
+        Me.bSave.Location = New System.Drawing.Point(303, 22)
+        Me.bSave.Name = "bSave"
+        Me.bSave.Size = New System.Drawing.Size(46, 23)
+        Me.bSave.TabIndex = 10
+        Me.bSave.Text = "Save"
+        Me.bSave.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(361, 271)
+        Me.Controls.Add(Me.bSave)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.lblSheetCode)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.lblCreatedAt)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.lstProducts)
+        Me.Controls.Add(Me.Button2)
+        Me.Name = "Form1"
+        Me.Text = "Basler to PLC"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents lstProducts As System.Windows.Forms.ListBox
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents lblCreatedAt As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents lblSheetCode As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents bSave As System.Windows.Forms.Button
+
+End Class
---
+++ baslertraficlight/Form1.vb
@@ -0,0 +1,103 @@
+﻿Imports System.Data.SqlClient
+Imports System.Configuration
+
+Public Class Form1
+
+    Dim oCmd As SqlClient.SqlCommand
+    Dim oDR As SqlClient.SqlDataReader
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+
+        '        SELECT [SheetCode] ,[Judgement] ,[CreatedAt] ,[Type] FROM [SNet].[dbo].[SheetJudgementResult]
+        '        WHERE [SheetCode] = (SELECT TOP 1 [SheetCode] FROM [SheetJudgementResult] ORDER BY [CreatedAt] DESC)
+
+
+        '        Try
+        ' oCmd = New SqlClient.SqlCommand()
+        ' With oCmd
+        ' .Connection = New SqlClient.SqlConnection(sConnectionString)
+        ' .Connection.Open()
+        ' .CommandText = sSelectQuery
+        ' oDR = .ExecuteReader()
+        ' End With
+
+        '        lstProducts.Items.Clear()
+        '        Do While oDR.Read()
+        ' lstProducts.Items.Add(oDR.Item("RUN"))
+        ' Loop
+        '
+        '        oDR.Close()
+        '        oCmd.Connection.Close()
+        '
+        '        Catch oExcept As Exception
+        ' MessageBox.Show(oExcept.Message)
+        '
+        '        End Try
+
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        Try
+            oDR = oCmd.ExecuteReader()
+            Do While oDR.Read()
+                'oDR.Item("RUN"))
+            Loop
+        Catch oExcept As Exception
+            'MessageBox.Show(oExcept.Message)
+            Debug.Print(oExcept.Message)
+        End Try
+
+    End Sub
+
+    Private Sub Form1_Disposed(sender As Object, e As EventArgs) Handles Me.Disposed
+        oDR.Close()
+        oCmd.Connection.Close()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Dim sConnectionString = "Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=RECIPE;Data Source=PROGRAMMING-PC;"
+        Dim sSelectQuery = "SELECT [SheetCode] ,[Judgement] ,[CreatedAt] ,[Type] FROM [SNet].[dbo].[SheetJudgementResult] WHERE [SheetCode] = (SELECT TOP 1 [SheetCode] FROM [SheetJudgementResult] ORDER BY [CreatedAt] DESC)"
+        Timer1.Enabled = False
+
+        Try
+            oCmd = New SqlClient.SqlCommand()
+            With oCmd
+                .Connection = New SqlClient.SqlConnection(sConnectionString)
+                .Connection.Open()
+                .CommandText = sSelectQuery
+            End With
+
+        Catch oExcept As Exception
+            MessageBox.Show(oExcept.Message)
+            End
+
+        End Try
+
+        Timer1.Interval = 15
+        Timer1.Enabled = True
+
+
+
+    End Sub
+
+    Private Sub bSave_Click(sender As Object, e As EventArgs) Handles bSave.Click
+
+    End Sub
+
+    Sub ReadSetting(key As String)
+        Try
+            Dim appSettings = ConfigurationManager.AppSettings
+            Dim result As String = appSettings(key)
+            If IsNothing(result) Then
+                result = "Not found"
+            End If
+            Console.WriteLine(result)
+        Catch e As ConfigurationErrorsException
+            Console.WriteLine("Error reading app settings")
+        End Try
+    End Sub
+End Class
